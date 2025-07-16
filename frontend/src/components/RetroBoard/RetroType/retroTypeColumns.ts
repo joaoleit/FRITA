3c828dd19d581cc1ea35cbb6ea85b52fb54fc10f
joaoleit <joao.vleite@ufrpe.br>
2025-07-16 00:25:37 -0300

    feat: :sparkles: add open the box retro board

---
+++ frontend/src/components/RetroBoard/RetroType/retroTypeColumns.ts
@@ -0,0 +1,26 @@
+import { RETROSPECTIVE_TYPES } from "../../../utils";
+import type { Columns } from "../types";
+
+const columnsMap: Record<RETROSPECTIVE_TYPES, Columns> = {
+  [RETROSPECTIVE_TYPES.WELL_NOT_SO_WELL]: {
+    well: { name: "Well" },
+    not_well: { name: "Not so Well" },
+    new_ideas: { name: "New Ideas" },
+  },
+  [RETROSPECTIVE_TYPES.OPEN_THE_BOX]: {
+    new_ideas: { name: "Novas Ideias" },
+    stop: { name: "Parar" },
+    recycle: { name: "Reciclar" },
+  },
+  [RETROSPECTIVE_TYPES.EASY_AS_PIE]: {
+    humblePie: { name: "Humble Pie" },
+    easyAsPie: { name: "Easy as Pie" },
+    cutiePie: { name: "Cutie Pie" },
+    pieInTheSky: { name: "Pie in the Sky" },
+    shooFlyPie: { name: "Shoo Fly Pie" },
+  },
+};
+
+export const getColumns = (type: RETROSPECTIVE_TYPES): Columns => {
+  return columnsMap[type] || columnsMap[RETROSPECTIVE_TYPES.WELL_NOT_SO_WELL];
+};
